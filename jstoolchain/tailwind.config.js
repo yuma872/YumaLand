903f4d12c747c1828ca9e3b2ac06de0fcff2678b
yuma872 <91675523+yuma872@users.noreply.github.com>
2022-10-13 13:57:42 +0600

    init

---
+++ jstoolchain/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      '../templates/base.html',
+      '../templates/main/main.html'
+
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
